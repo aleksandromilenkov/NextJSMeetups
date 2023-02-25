8ba3b37fad22ac38b7e05eed53804d2be1d04a03
Macedonian-MKD <macedonian.macedon@yahoo.com>
2023-02-25 16:24:46 +0100

    Fixing meetup page

--- src/pages/[meetupId]/index.js
+++ src/pages/[meetupId]/index.js
@@ -36,7 +36,7 @@ export async function getStaticPaths() {
         meetupId: meetup._id.toString(),
       },
     })),
-    fallback: false,
+    fallback: "blocking",
   };
 }
 export async function getStaticProps(context) {
